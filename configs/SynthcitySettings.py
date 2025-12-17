8b5696c90a15683d30287d33cd8a4867ba38007a
Vassilis Papastergios <papster@csd.auth.gr>
2025-12-18 00:24:30 +0200

    Add generator class for synthcity models and centralize management of available options

---
+++ configs/SynthcitySettings.py
@@ -0,0 +1,29 @@
+# TODO: I know we want something quick and dirty for now, 
+# but for later on we could consider unifying the Settings instead of
+# having one for TabPFN and another for Synthcity
+
+from enum import Enum
+
+class SynthcityModelOption(Enum):
+    TVAE = 'tvae'
+    CTGAN = 'ctgan'
+    NFLOW = 'nflow'
+    DDPM = 'ddpm'
+    ARF = 'arf'
+    MARGINAL_DISTRIBUTIONS = 'marginal_distributions'
+
+class SynthcitySettings:
+    def __init__(
+        self, 
+        model_name: SynthcityModelOption,
+        n_samples: int = 1000,
+    ):
+        self.model_name = model_name.value
+        self.n_samples = n_samples
+
+    def to_dict(self) -> dict:
+        return vars(self)
+
+    @classmethod
+    def from_dict(cls, settings_dict: dict):
+        return cls(**settings_dict)
