f5be9a8b4da2fd513c8c81c2ace9b24a76058d3a
Vassilis Papastergios <papster@csd.auth.gr>
2026-01-19 11:04:59 +0200

    Update Postgres schema

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,11 +1,25 @@
--- TODO: This needs to be updated when we decide on the DB schema
-CREATE TABLE IF NOT EXISTS model_metrics (
+CREATE TABLE IF NOT EXISTS errors (
     id SERIAL PRIMARY KEY,
-    dataset_id VARCHAR(50),
-    model_name VARCHAR(50),
-    metric_name VARCHAR(50),
-    metric_value FLOAT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    experiment_id VARCHAR(255) NOT NULL,
+    file_path TEXT NOT NULL,
+    error_message TEXT,
+    execution_profile VARCHAR(20),
+    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Athens')
 );
 
-CREATE INDEX idx_dataset_model ON model_metrics(dataset_id, model_name);
+CREATE TABLE IF NOT EXISTS evaluation_results (
+    id SERIAL PRIMARY KEY,
+    experiment_id VARCHAR(255) NOT NULL,
+    first_input_file_path VARCHAR(120) NOT NULL,
+    second_input_file_path VARCHAR(120),
+    evaluation_shortname VARCHAR(10) NOT NULL,
+    random_seed VARCHAR(10) NOT NULL,
+    error_type VARCHAR(10) NOT NULL,
+    error_rate VARCHAR(3) NOT NULL,
+    result NUMERIC NOT NULL,
+    notes JSONB,
+    execution_profile VARCHAR(20),
+    created_at TIMESTAMP DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'Europe/Athens')
+);
+
+CREATE INDEX idx_seed_evaluation_shortname ON evaluation_results(evaluation_shortname, random_seed)
